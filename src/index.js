74a08dcd0fdcbd5baed462d05db487d08d2ea537
pnam <phuongnamhn2000@gmail.com>
2021-05-15 14:13:21 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import {Client} from "discord.js";
+
+import schedule from "node-schedule";
+import moment from "moment-timezone";
+
+import {getCovidData, processCommand} from "./services.js";
+import {token} from "../secrets.js"
+
+// set timezone
+moment.tz.setDefault("Asia/Ho_Chi_Minh");
+
+const client = new Client();
+
+
+
+//Check kết nối discord
+client.on("ready", () => {
+  client.user.setActivity("DISCORD", {type: "STREAMING"});
+
+  // Vòng FOR duyệt guilds và các channels
+  // client.guilds.cache.forEach((guild) => {
+  //   console.log(guild.name);
+  //   guild.channels.cache.forEach((channel) => {
+  //     console.log(` - ${channel.name} ${channel.type} ${channel.id}`);
+  //   });
+  // });
+
+  const channel = client.channels.cache.get("841682021218254858");
+
+  // const attach = new Discord.MessageAttachment("./image/image.jpg");
+  // channel.send("hihi");
+  // channel.send(attach);
+
+  // */6 * * *          // 6h
+  // "*/3 * * * * *"    // 3s
+
+  const rule = new schedule.RecurrenceRule();
+
+  rule.hour = [5, 11, 17];
+  rule.second = 0;
+
+  schedule.scheduleJob(
+    // "*/6 * * *",
+    rule,
+    async () => {
+      channel.send(await getCovidData());
+    });
+
+  // job;
+});
+
+client.on("message", async(message) => {
+
+  if (
+    message.author === client.user ||
+    message.member.roles.cache.some((role) => role.name === "bot") ||
+    message.channel.name !== "phuong_nam"
+  ) return;
+
+  message.react("✅");
+  message.react("😜");
+
+  if (message.content.substr(0, 5) === "hit::") {
+    processCommand(message);
+  }
+  // message.channel.send(await Covid());
+
+});
+
+client.login(token);
---
+++ src/services.js
@@ -0,0 +1,124 @@
+import axios from "axios";
+import moment from "moment-timezone";
+
+import {MessageAttachment} from "discord.js";
+import {readdirSync} from "fs";
+
+export const ShowImage = (receivedMessage) => {
+  const img = readdirSync("image/"); // Lấy tất cả ảnh thành một list trong folder Image/Anime
+
+  // Lấy random ảnh anime
+  const attachment = new MessageAttachment("image/" + img[0]); // Lấy ảnh đầu trong thư mục
+  receivedMessage.channel.send(attachment);
+};
+
+export const processCommand = (receivedMessage) => {
+  let fullCommand = receivedMessage.content
+    .substr(5, receivedMessage.content.length);
+  if (fullCommand==null || fullCommand =="") return;
+  let splitCommand = fullCommand.split(" ");
+  let cmd = splitCommand[0];
+  let args = splitCommand.slice(1);
+  let firstArg = splitCommand[1];
+  let secondArg = splitCommand[2];
+  let thirdArg = splitCommand[3];
+
+  console.log(splitCommand);
+  console.log("Command received: " + cmd);
+  console.log("Arguments: " + args);
+  console.log("Arguments 1: " + firstArg);
+  console.log("Arguments 2: " + secondArg);
+  console.log("Arguments 3: " + thirdArg);
+
+  switch(cmd)
+  {
+  case "covid":
+    Covid(receivedMessage);
+    break;
+  default:
+    receivedMessage.channel.send("Bot không hỉu, bot không hỉu, bot không hỉu");
+    break;
+  }
+};
+
+const Covid = async (receivedMessage) => {
+  const data = await getCovidData();
+  receivedMessage.channel.send(data);
+};
+
+const covidUrl = "https://corona.lmao.ninja/v2/countries/vn";
+const fetchApi = () => axios.get(covidUrl);
+
+export const getCovidData = async () => {
+  const {data} = await fetchApi();
+  const {
+    updated,
+    country,
+    cases,
+    todayCases,
+    deaths,
+    todayDeaths,
+    recovered,
+    todayRecovered,
+    population,
+    continent,
+    undefined: undefine,
+    // active,
+    // critical,
+    // casesPerOneMillion,
+    // deathsPerOneMillion,
+    // tests,
+    // testsPerOneMillion,
+    // oneCasePerPeople,
+    // oneDeathPerPeople,
+    // oneTestPerPeople,
+    // activePerOneMillion,
+    // recoveredPerOneMillion,
+    // criticalPerOneMillion,
+    // countryInfo,
+  } = data;
+
+  // console.log(
+  //   `Quốc gia: ${country}
+  //   Khu vực: ${continent}
+  //   Dân số: ${population}
+  //   Số ca nhiễm: ${cases}
+  //   Số ca nhiễm trong ngày hôm nay: ${todayCases}
+  //   Số ca tử vong: ${deaths}
+  //   Số ca tử vong ngày hôm nay: ${todayDeaths}
+  //   Số ca đã phục hồi: ${recovered}
+  //   Số ca đã phục hồi trong ngày hôm nay: ${todayRecovered}
+  //   Số ca chưa xác định: ${undefined}
+  //   Dữ liệu được cập nhật lúc: ${updated}
+  //   // Ngay hom nay co: ${tests} // chắc là số liều vacin
+  //   // Ngay hom nay co: ${active}
+  //   // Ngay hom nay co: ${critical}
+  //   // Ngay hom nay co: ${casesPerOneMillion}
+  //   // Ngay hom nay co: ${deathsPerOneMillion}
+  //   // Ngay hom nay co: ${testsPerOneMillion}
+  //   // Ngay hom nay co: ${oneCasePerPeople}
+  //   // Ngay hom nay co: ${oneDeathPerPeople}
+  //   // Ngay hom nay co: ${oneTestPerPeople}
+  //   // Ngay hom nay co: ${activePerOneMillion}
+  //   // Ngay hom nay co: ${recoveredPerOneMillion}
+  //   // Ngay hom nay co: ${criticalPerOneMillion}
+  //   // Ngay hom nay co: ${countryInfo}
+  //    `,
+  // );
+
+  return `============== COVID 19 ==============
+          Quốc gia: ${country}
+          Khu vực: ${continent}
+          Dân số: ${population}
+          Số ca nhiễm: ${cases}
+          Số ca nhiễm trong ngày hôm nay: ${todayCases}
+          Số ca tử vong: ${deaths}
+          Số ca tử vong ngày hôm nay: ${todayDeaths}
+          Số ca đã phục hồi: ${recovered}
+          Số ca đã phục hồi trong ngày hôm nay: ${todayRecovered}
+          Số ca chưa xác định: ${undefine}
+          Dữ liệu được cập nhật lúc: ${moment(updated)
+    .locale("vi")
+    .format("h:mm:ss, DD/MM/YYYY")}
+          `;
+};
